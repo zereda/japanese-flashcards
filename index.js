a6d76a06b5ae6bf85b07769f4e0251bd101f8d4d
zereda <zereda.zy@gmail.com>
2017-08-11 17:50:19 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,50 @@
+var flashcard = document.getElementById ("flashcard");
+var kanjidiv= document.getElementById("kanji");
+var worddiv = document.getElementById("word");
+var meaningdiv= document.getElementById("meaning");
+var button= document.getElementById ("btn1");
+var backbutton = document.getElementById ("btn2");
+
+var i = 0;
+var IdInterval = null;
+
+showkanjicard();
+
+button.addEventListener( "click", () => {
+
+	if (IdInterval === null){
+
+		IdInterval = setInterval(function(){
+			i = (i + 1) % kanjicards.length;
+			showkanjicard();
+			
+		}, 1000);
+		btn1.textContent = "Pause";
+	}
+	else{
+		stopAuto();
+	}
+});
+
+function stopAuto() {
+	clearInterval(IdInterval);
+	IdInterval= null;
+	btn1.textContent = "Start";
+}
+
+backbutton.addEventListener("click",()=>{
+	stopAuto();
+	
+	if( i == 0) {
+		return;
+	}
+
+	i = (i - 1) % kanjicards.length;
+	showkanjicard();
+});
+
+function showkanjicard(){
+	kanjidiv.textContent = kanjicards[i].kanji; 
+	worddiv.textContent = kanjicards[i].word;
+	meaningdiv.textContent = kanjicards[i].meaning;
+}
